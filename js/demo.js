792874cc04e294aea8941729ab9ea278dfd77ced
Peter Zam <peterzam.369@gmail.com>
2020-06-08 21:16:29 +0630

    Add files via upload

--- js/demo.js
+++ js/demo.js
@@ -6,14 +6,20 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#E83951',
+    lineColor: '#a8dadc'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
 
-
+$(document).ready(function(){
+	$('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},1500);
+	});
+});
 /*
 // jQuery plugin example:
 $(document).ready(function() {
